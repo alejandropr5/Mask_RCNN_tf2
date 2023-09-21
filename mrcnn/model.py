0c2ed7e77a7ab242f071f8b74e08aa24fdf2e3bc
alejandropr05 <alejandropr05@hotmail.com>
2023-09-20 21:16:49 -0500

    set workers=1, and use_multiprocessing=False

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2363,8 +2363,8 @@ class MaskRCNN(object):
             validation_data=val_generator,
             validation_steps=self.config.VALIDATION_STEPS,
             max_queue_size=100,
-            workers=workers,
-            use_multiprocessing=workers > 1,
+            workers=1,
+            use_multiprocessing=False,
         )
         self.epoch = max(self.epoch, epochs)
 
