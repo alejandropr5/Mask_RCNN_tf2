ecee0765a1fb01d5e054739fd4b7a96e37949bdc
alejandropr05 <alejandropr05@hotmail.com>
2023-09-20 21:39:41 -0500

    update keras optimizer method

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2153,8 +2153,8 @@ class MaskRCNN(object):
         metrics. Then calls the Keras compile() function.
         """
         # Optimizer object
-        optimizer = keras.optimizers.SGD(
-            lr=learning_rate, momentum=momentum,
+        optimizer = keras.optimizers.legacy.SGD(
+            learning_rate=learning_rate, momentum=momentum,
             clipnorm=self.config.GRADIENT_CLIP_NORM)
         # Add Losses
         loss_names = [
